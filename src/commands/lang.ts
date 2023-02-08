d7183eaa00475e628611e457a669dc47578c9806
Andndre <agungandre687@gmail.com>
2023-02-08 18:32:15 +0800

    refactor: syntax mistake

--- src/commands/lang.ts
+++ src/commands/lang.ts
@@ -4,47 +4,45 @@ import { findConv, prisma, updateLang } from "../services/prisma.js";
 import { Ctx } from "../utils/types.js";
 
 export async function lang(ctx: Ctx) {
-  {
-    const [_, newLang = ""] = ctx.message.text.split(" ");
-    if (!newLang) {
-      ctx.reply(
-        messages.invalidLangUsage,
-      );
-      return;
-    }
-
-    const validator = /^[a-z]{2,3}(-[A-Z]{2,3}){0,1}$/;
-
-    if (!validator.test(newLang)) {
-      ctx.reply(
-        messages.invalidIETF,
-      );
-      return;
-    }
-
-    const chatId = ctx.chat.id + "id";
-    const conv = await findConv(chatId);
-    const reply = await chatGPT(
-      `Untuk seterusnya, respon menggunakan bahasa "${newLang}"`,
-      ctx.chat.id,
-      conv?.conversationId,
-      conv?.prevMessageId,
+  const [_, newLang = ""] = ctx.message.text.split(" ");
+  if (!newLang) {
+    ctx.reply(
+      messages.invalidLangUsage,
     );
-    if (!reply) {
-      ctx.reply(messages.unknownError);
-      return;
-    }
-
-    await Promise.all([
-      ctx.reply(reply.text),
-      conv ? updateLang(chatId, newLang) : prisma.conversation.create({
-        data: {
-          chatId,
-          lang: newLang,
-          conversationId: reply.conversationId!,
-          prevMessageId: reply.id,
-        },
-      }),
-    ]);
+    return;
   }
+
+  const validator = /^[a-z]{2,3}(-[A-Z]{2,3}){0,1}$/;
+
+  if (!validator.test(newLang)) {
+    ctx.reply(
+      messages.invalidIETF,
+    );
+    return;
+  }
+
+  const chatId = ctx.chat.id + "id";
+  const conv = await findConv(chatId);
+  const reply = await chatGPT(
+    `Untuk seterusnya, respon menggunakan bahasa "${newLang}"`,
+    ctx.chat.id,
+    conv?.conversationId,
+    conv?.prevMessageId,
+  );
+  if (!reply) {
+    ctx.reply(messages.unknownError);
+    return;
+  }
+
+  await Promise.all([
+    ctx.reply(reply.text),
+    conv ? updateLang(chatId, newLang) : prisma.conversation.create({
+      data: {
+        chatId,
+        lang: newLang,
+        conversationId: reply.conversationId!,
+        prevMessageId: reply.id,
+      },
+    }),
+  ]);
 }
