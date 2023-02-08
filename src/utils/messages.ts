814e73a78429e25e1710a16bda4e5f2d8cc7dafd
Andndre <agungandre687@gmail.com>
2023-02-08 18:26:21 +0800

    fix: wrong IETF example

--- src/utils/messages.ts
+++ src/utils/messages.ts
@@ -2,7 +2,7 @@ export const messages = {
   unknownError: "Something went wrong, please try again later...",
   hello: "Hello! I'm ChatGPT.. how can I help you?",
   invalidLangUsage:
-    'To set new default language, use "/lang [IETF_LangTag]"\n\nIETF_LangTag example:\nid-ID\nen-EN\n\n\n\nMore about IETF Language Tag: https://en.wikipedia.org/wiki/IETF_language_tag',
+    'To set new default language, use "/lang [IETF_LangTag]"\n\nIETF_LangTag example:\nid-ID\nen-US\n\nMore about IETF Language Tag: https://en.wikipedia.org/wiki/IETF_language_tag',
   invalidIETF:
     "Invalid IETF_LangTag!\n\nexample (for Indonesian): /lang id-ID\n\nMore about IETF Language Tag: https://en.wikipedia.org/wiki/IETF_language_tag",
 } as const;
