3fb5d2617280da77ef94633aa814ef08e8cab706
Andndre <agungandre687@gmail.com>
2023-02-08 18:36:04 +0800

    fix: add error handling on voice

--- src/events/voice.ts
+++ src/events/voice.ts
@@ -6,27 +6,33 @@ import { findConv } from "../services/prisma.js";
 import { VoiceCtx } from "../utils/types.js";
 
 export async function voice(ctx: VoiceCtx) {
-  const conv = await findConv(ctx.chat.id + "id");
-  const fileId = ctx.message.voice.file_id;
-  const fileUrl = await ctx.telegram.getFileLink(fileId);
-  const voiceMessage = await got(fileUrl, { responseType: "buffer" });
-  ctx.sendChatAction("typing");
-  const transcription = await transcribe(
-    voiceMessage.body,
-    conv?.lang || "id-ID",
-  );
-  await Promise.all([
-    ctx.reply("You said: " + transcription + "\n\nGetting ChatGPT's answer..."),
-    ctx.sendChatAction("typing"),
-    new Promise<void>(async (resolve) => {
-      const reply = await chatGPT(
-        transcription,
-        ctx.chat.id,
-        conv?.conversationId,
-        conv?.prevMessageId,
-      );
-      await ctx.reply(reply?.text || messages.unknownError);
-      resolve();
-    }),
-  ]);
+  try {
+    const conv = await findConv(ctx.chat.id + "id");
+    const fileId = ctx.message.voice.file_id;
+    const fileUrl = await ctx.telegram.getFileLink(fileId);
+    const voiceMessage = await got(fileUrl, { responseType: "buffer" });
+    ctx.sendChatAction("typing");
+    const transcription = await transcribe(
+      voiceMessage.body,
+      conv?.lang || "id-ID",
+    );
+    await Promise.all([
+      ctx.reply(
+        "You said: " + transcription + "\n\nGetting ChatGPT's answer...",
+      ),
+      ctx.sendChatAction("typing"),
+      new Promise<void>(async (resolve) => {
+        const reply = await chatGPT(
+          transcription,
+          ctx.chat.id,
+          conv?.conversationId,
+          conv?.prevMessageId,
+        );
+        await ctx.reply(reply?.text || messages.unknownError);
+        resolve();
+      }),
+    ]);
+  } catch (e: any) {
+    ctx.reply(messages.unknownError + "\n\n" + e.message);
+  }
 }
